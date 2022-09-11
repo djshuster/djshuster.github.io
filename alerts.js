7d9ce763c1dd69102b0a87ec93f03eb77b10d68e
Platypus Biped <djs.shuster@gmail.com>
2022-09-10 23:10:17 -0500

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,2 +1,2 @@
-	alert("I didn't give you a virus." );
+	alert("I didn't give you a virus. Really." );
 	alert("Uh oh, I see you've got an ad blocker on. It's ok, though; ads are often frankly unethical and terrible insofar as they lead astray, for monetary gain, the desires of people.")
